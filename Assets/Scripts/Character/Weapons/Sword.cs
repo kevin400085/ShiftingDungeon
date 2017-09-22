195ee451aebe4e77d88bdcc7a9d8c61e2e312061
kevin400085 <kevin400085@yahoo.com>
2017-09-22 00:30:10 -0400

    Revert "Traps"

--- Assets/Scripts/Character/Weapons/Sword.cs
+++ Assets/Scripts/Character/Weapons/Sword.cs
@@ -8,8 +8,6 @@
         [SerializeField]
         private float swingSpeed = 400f;
         [SerializeField]
-        private float lungeForce = 200f;
-        [SerializeField]
         private float arcLength = 120f;
         [SerializeField]
         private SoundPlayer sfx;
@@ -17,17 +15,11 @@
         private ContactDamage damageDealer;
 
         private bool doOnce;
-        private float arc;
-
-        private SpriteRenderer spriteRenderer;
-        private Rigidbody2D playerBody;
+        private float arc;        
 
         protected override void LocalInit()
         {
-            this.playerBody = GetComponentInParent<Rigidbody2D>();
-            this.spriteRenderer = GetComponentInChildren<SpriteRenderer>();
             this.arc = 0;
-            this.maxCombo = 3;
         }
 
         protected override void LocalReInit()
@@ -36,34 +28,6 @@
             this.arc = 0;
             this.doOnce = false;
             this.damageDealer.level = this.Level;
-
-            Vector3 spritePosition;
-            switch (comboCounter)
-            {
-                //Lunge left
-                case 1:
-                    this.spriteRenderer.flipY = true;
-                    spritePosition = this.transform.GetChild(0).transform.localPosition;
-                    spritePosition.y = -Mathf.Abs(spritePosition.y);
-                    this.transform.GetChild(0).transform.localPosition = spritePosition;
-                    this.playerBody.AddForce(playerBody.transform.right * lungeForce);
-                    break;
-                //Lunge right
-                case 2:
-                    this.spriteRenderer.flipY = false;
-                    spritePosition = this.transform.GetChild(0).transform.localPosition;
-                    spritePosition.y = Mathf.Abs(spritePosition.y);
-                    this.transform.GetChild(0).transform.localPosition = spritePosition;
-                    this.playerBody.AddForce(playerBody.transform.right * lungeForce);
-                    break;
-                //Swing right
-                default:
-                    this.spriteRenderer.flipY = false;
-                    spritePosition = this.transform.GetChild(0).transform.localPosition;
-                    spritePosition.y = Mathf.Abs(spritePosition.y);
-                    this.transform.GetChild(0).transform.localPosition = spritePosition;
-                    break;
-            }
         }
 
         public override bool WeaponUpdate()
@@ -74,28 +38,9 @@
                 this.doOnce = true;
             }
 
-
-            
-            switch (comboCounter)
-            {
-                //Lunge left
-                case 1:
-                    this.arc += Time.deltaTime * (this.swingSpeed * 2 + (50f * this.Level));
-                    this.transform.localRotation = Quaternion.Euler(0, 0, this.arc);
-                    return this.arc >= this.arcLength;
-                //Lunge right
-                case 2:
-                    this.arc += Time.deltaTime * (this.swingSpeed * 2 + (50f * this.Level));
-                    this.transform.localRotation = Quaternion.Euler(0, 0, -this.arc);
-                    return this.arc >= this.arcLength;
-                //Swing right
-                default:
-                    this.arc += Time.deltaTime * (this.swingSpeed + (50f * this.Level));
-                    this.transform.localRotation = Quaternion.Euler(0, 0, -this.arc);
-                    return this.arc >= this.arcLength;
-            }
-
-            
+            this.arc += Time.deltaTime * (this.swingSpeed + (50f * this.Level));
+            this.transform.localRotation = Quaternion.Euler(0, 0, -this.arc);
+            return this.arc >= this.arcLength;
         }
 
         protected override void LocalCleanUp()
